268d7e02d7169126da636dc122c2a7ba6cebebf9
arnau <amarques20733@iesjoanramis.org>
2025-02-06 21:11:03 +0100

    accuweather contesta

--- dwes/pages/accu_weather.js
+++ dwes/pages/accu_weather.js
@@ -1,6 +1,21 @@
 
 
-    let location_key = "305476"
-    let request = "http://dataservice.accuweather.com/currentconditions/v1/{$location_key}"
+    let location_key = "305476";
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    let request = 'http://dataservice.accuweather.com/currentconditions/v1/305476?apikey=y9YjUYyZu62bklgymt0NecThs5NQvCq2&language=es-es'
+    let json;
 
-    let xhttp;
+    let xhttp = new  XMLHttpRequest();
+    xhttp.onload = function () {  
+        json = this.responseText;
+        console.log(JSON.parse(json));
+    }
+    xhttp.open("GET", request,true);
+    xhttp.send()
+   
+
+
+    // fetch('http://dataservice.accuweather.com/currentconditions/v1/305476?apikey=y9YjUYyZu62bklgymt0NecThs5NQvCq2&language=es-es')
+    // .then(response => response.json())
+    // .then(json => console.log(json))
+    // .catch(error => console.error('Error:', error));
